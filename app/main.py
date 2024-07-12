f3bc565bcde564ef792d342e43dde1e792dd3bff
Abhinav <abhiasthana76@gmail.com>
2024-07-12 13:14:18 +0530

    test

--- app/main.py
+++ app/main.py
@@ -8,32 +8,32 @@ def main():
 
     # Uncomment this to pass the first stage
     #
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
     # server_socket.accept() # wait for client
-    connection, address = server_socket.accept()
+        connection, address = server_socket.accept()
 
-    with connection:
-        while True:
-            # recieves data from the connection
-            data = connection.recv(1024)
-            # print(f"Received Data: {data} ")
+        with connection:
+            while True:
+                # recieves data from the connection
+                data = connection.recv(1024)
+                # print(f"Received Data: {data} ")
 
-            # decode data in utf-8 format by default
-            request_data = data.decode().split("\r\n")
-            # print(f"Decoded Data: {decode_data}")
+                # decode data in utf-8 format by default
+                request_data = data.decode().split("\r\n")
+                # print(f"Decoded Data: {decode_data}")
 
-            if not data:
-                break
+                if not data:
+                    break
 
-            request_path = request_data[0].split(" ") 
-            # print(f"Requested Path: {request_path}")
+                request_path = request_data[0].split(" ") 
+                # print(f"Requested Path: {request_path}")
             
-            request_string = request_path[1].split()
-            # Check condtion
-            if request_string[0] == "/":
-                connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
-            elif request_string[0] != "/":
-                connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+                request_string = request_path[1].split()
+                # Check condtion
+                if request_string[0] == "/":
+                    connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                elif request_string[0] != "/":
+                    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
