e6ba5186f946fe1def8f03e769b679dc3d1ddcfd
Abhinav <abhiasthana76@gmail.com>
2024-07-12 11:33:17 +0530

    extract URL path

--- app/main.py
+++ app/main.py
@@ -11,7 +11,11 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     # server_socket.accept() # wait for client
     connection, address = server_socket.accept()
-    connection.sendall(b"GET /index.html HTTP/1.1\r\nHost: localhost:4221\r\nUser-Agent: curl/7.64.1\r\nAccept: */*\r\n\r\n")
+    if address == "http://localhost:4221/index.html":
+        connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+    else:
+        print(f"Accepted connection {connection} from {address}")
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
     main()
