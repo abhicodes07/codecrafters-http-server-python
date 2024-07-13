971c780cc24e95bb5caa25ce5b3c968b829e50bd
Abhinav <abhiasthana76@gmail.com>
2024-07-13 15:53:22 +0530

    respond with body

--- app/main.py
+++ app/main.py
@@ -25,6 +25,8 @@ def main():
                     break
                 
                 request_path = request_data[1].split("/")
+                print(f"Request Data: {request_data}")
+                print(f"Request Path: {request_path}")
                 
                 if len(request_path) >= 3 and request_path[1] == "echo":
                     content = request_path[2]
@@ -34,7 +36,7 @@ def main():
                 elif len(request_path)==2:
                     connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
                 
-                elif request_data[1] == "/":
+                else:
                     connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 if __name__ == "__main__":
