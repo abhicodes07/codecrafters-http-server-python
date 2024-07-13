4965b69ab415d2d83c650db68a714ed20f6d1cca
Abhinav <abhiasthana76@gmail.com>
2024-07-13 14:36:09 +0530

    respond with body

--- app/main.py
+++ app/main.py
@@ -17,12 +17,19 @@ def main():
                 # recieves data from the connection
                 data = connection.recv(1024)
                 # decode data in utf-8 format by default
+                
+                # 2. sent- HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 3\r\n\r\nabc
                 request_data = data.decode().split(" ")
+                
                 if not data:
                     break
-                request_path = request_data[1] 
-                if request_path[0] == "/" and len(request_path)>1:
+                request_path = request_data[1]
+
+                if request_path[0] == "/" and len(request_path)>1 and request_path[1:5] != "echo":
                     connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+                elif request_path[0:6] == "/echo/":
+                    content_length = request_path[6:]
+                    connection.sendall(b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {content_length}\r\n\r\nabc".format(content_length))
                 else:
                     connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
