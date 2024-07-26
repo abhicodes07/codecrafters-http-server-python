86f7395b3cae411d41463f041079c27cb9480789
Abhinav <abhiasthana76@gmail.com>
2024-07-26 13:55:54 +0530

    test

--- app/main.py
+++ app/main.py
@@ -50,6 +50,10 @@ def handle_requests(connection, address):
                     connection.sendall(file_response.encode())
                 except Exception as e:
                     connection.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+            elif request_path[1] != '' and request_path[1] == "POST":
+                return 
+                with open(p, "wb") as out:
+                    out.write()
 
             else:
                 connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
